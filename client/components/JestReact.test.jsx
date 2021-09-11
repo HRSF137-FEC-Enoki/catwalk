025405b8d5135d1701069fbb6bb3ca58d9ff235d
jleiandy <jleiandy@gmail.com>
2021-09-10 17:44:16 -0700

    installed react test library and refactored sample tests

---
+++ client/components/JestReact.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './JestComponent';
+import '@testing-library/jest-dom';
+
+
+beforeEach(() => {
+  render(<App />);
+});
+
+describe('App Component', () => {
+  test('Header Contains Greeting', () => {
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello from the React App');
+  });
+  test('Should render one button', () => {
+    expect(screen.queryAllByRole('button')).toHaveLength(1);
+  });
+  test('Initial count value should be 0', () => {
+    expect(screen.getByRole('button')).toHaveTextContent(0);
+  });
+  test('Clicking button should increment count value by 1', () => {
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('button')).toHaveTextContent(1);
+    userEvent.click(screen.getByRole('button'));
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('button')).toHaveTextContent(3);
+  });
+});
