0345c8d8846699337c7895fe8b9f3122011a16e5
jleiandy <jleiandy@gmail.com>
2021-09-11 00:56:50 -0700

    Fixed linting Fixed linting in import statements in JestReact file and also fixed linting in Jest.config file

--- client/components/JestReact.test.jsx
+++ client/components/JestReact.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './JestComponent';
-import '@testing-library/jest-dom';
 
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from './JestComponent';
 
 beforeEach(() => {
   render(<App />);
