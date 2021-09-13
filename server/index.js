d559770e2f9c2e915eb9681cb441de4da001829c
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-13 12:38:16 -0700

    Add additional route for single product and remove body parser

--- server/index.js
+++ server/index.js
@@ -1,32 +1,44 @@
-/* eslint-disable arrow-parens */
 const express = require('express');
-
-const app = express();
-const bodyParser = require('body-parser');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
 const path = require('path');
 const axios = require('axios');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
 
 const port = 3000;
 
 const config = require('../config/config');
 
+const PRODUCTS_URL = `${config.API_URL}products`;
+const options = {
+  headers: { Authorization: config.GITHUB_API_TOKEN },
+};
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/products', (req, res) => {
-  axios.get(`${config.API_URL}products`, {
-    headers: { Authorization: config.GITHUB_API_TOKEN },
-  })
-    .then(response => {
+  axios.get(PRODUCTS_URL, options)
+    .then((response) => {
       res.send(response.data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.send(`Error:: ${err}`);
+    });
+});
+
+app.get('/products/:product_id', (req, res) => {
+  const productId = req.params.product_id;
+
+  axios.get(`${PRODUCTS_URL}/${productId}`, options)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
     });
 });
 
