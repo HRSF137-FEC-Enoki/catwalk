035d4770d2344aa3be85b85905014d2e93cdd02f
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-13 15:57:13 -0700

    Create related products module with endpoint

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,43 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
+import axios from 'axios';
 
-const App = () => (<div>Hello from the React App</div>);
+import RelatedProducts from './RelatedProducts';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      currentProduct: null,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/products')
+      .then(({ data }) => {
+        this.setState({
+          products: data,
+          currentProduct: data[0],
+          isLoading: false,
+        });
+      });
+  }
+
+  render() {
+    const { currentProduct, isLoading } = this.state;
+
+    return (
+      <div className="app-container">
+        <header>
+          Logo and Search Go Here
+        </header>
+        {isLoading ? <p>Loading!</p> : <RelatedProducts productId={currentProduct.id} />}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ client/components/RelatedProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RelatedProducts = (props) => {
+  const { productId } = props;
+
+  console.log(productId);
+
+  return <div>RelatedProducts</div>;
+};
+
+export default RelatedProducts;
