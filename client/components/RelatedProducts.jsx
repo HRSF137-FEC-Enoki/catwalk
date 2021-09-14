8b957c484f0704f41d6e956e60745319345c7025
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-13 19:32:46 -0700

    Getting data for related products module

--- client/components/RelatedProducts.jsx
+++ client/components/RelatedProducts.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const RelatedProducts = (props) => {
-  const { productId } = props;
+const RelatedProducts = ({ productId }) => {
+  const [products, setProducts] = useState([]);
 
-  console.log(productId);
+  useEffect(() => {
+    // make a GET request to /api/products/:product_id/related
+    axios.get(`/api/products/${productId}/related`)
+      .then(({ data }) => data)
+      .then((ids) => ids.map((id) => axios.get(`/api/products/${id}`)))
+      .then((promises) => {
+        Promise.all(promises).then((values) => setProducts(values));
+      });
+  }, []);
 
-  return <div>RelatedProducts</div>;
+  return (
+    <div>
+      <h3>RelatedProducts</h3>
+      {products.map(({ data }) => {
+        return <div key={data.id}>{data.name}</div>;
+      })}
+    </div>
+  );
 };
 
 export default RelatedProducts;
