6b708bcb684fb4b40bf2b8fff66efb6828d8a4e0
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-13 19:41:14 -0700

    Refactor App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,43 +1,28 @@
-/* eslint-disable react/no-unused-state */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import RelatedProducts from './RelatedProducts';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  // const [products, setProducts] = useState([]);
+  const [currentProduct, setCurrentProduct] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    this.state = {
-      products: [],
-      currentProduct: null,
-      isLoading: true,
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios.get('/api/products')
       .then(({ data }) => {
-        this.setState({
-          products: data,
-          currentProduct: data[0],
-          isLoading: false,
-        });
+        // setProducts(data);
+        setCurrentProduct(data[0]);
+        setIsLoading(false);
       });
-  }
+  }, []);
 
-  render() {
-    const { currentProduct, isLoading } = this.state;
-
-    return (
-      <div className="app-container">
-        <header>
-          Logo and Search Go Here
-        </header>
-        {isLoading ? <p>Loading!</p> : <RelatedProducts productId={currentProduct.id} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app-container">
+      <header>Logo and Search Go Here</header>
+      {isLoading ? <p>Loading!</p> : <RelatedProducts productId={currentProduct.id} />}
+    </div>
+  );
+};
 
 export default App;
