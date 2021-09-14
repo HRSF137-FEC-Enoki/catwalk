cabc3e543d3249d8cb9c08ff71b706374cb7056d
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-14 08:48:28 -0700

    Mock out card component

--- client/components/relatedProducts/Card.jsx
+++ client/components/relatedProducts/Card.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { AiOutlineStar } from 'react-icons/ai';
 
-const Card = ({ relatedProduct }) => {
-  console.log('rel:: ', relatedProduct);
-  return <div>Card goes here</div>;
-};
+const Card = ({ relatedProduct }) => (
+  <div className="related-products__card">
+    <div className="related-products__card-image">
+      <AiOutlineStar />
+    </div>
+    <p className="related-products__card-category">{relatedProduct.category}</p>
+    <p className="related-proucts__card-expanded-name">
+      {`${relatedProduct.name} - ${relatedProduct.slogan}`}
+    </p>
+    <p className="related-products__card-price">{relatedProduct.default_price}</p>
+    <p className="related-products__card-rating">TODO:: Star Rating</p>
+  </div>
+);
 
 Card.propTypes = {
-
+  relatedProduct: PropTypes.shape({
+    name: PropTypes.string,
+    slogan: PropTypes.string,
+    category: PropTypes.string,
+    default_price: PropTypes.string,
+  }).isRequired,
 };
 
 export default Card;
