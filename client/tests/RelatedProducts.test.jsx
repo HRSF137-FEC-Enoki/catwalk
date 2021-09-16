bbcd2d309c68bea9d66479183407b3b2337bc56f
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-16 11:13:01 -0700

    Update jest config to use runtime generator

--- client/tests/RelatedProducts.test.jsx
+++ client/tests/RelatedProducts.test.jsx
@@ -5,15 +5,19 @@ import '@testing-library/jest-dom';
 import RelatedProducts from '../components/relatedProducts/RelatedProducts';
 
 describe('Related Products', () => {
+  const mockProductId = 43432;
+
   beforeEach(() => {
-    render(<RelatedProducts productId={43432} />);
+    render(<RelatedProducts productId={mockProductId} />);
   });
 
   test('it should have a header element', () => {
     expect(screen.getByRole('heading')).toHaveTextContent('Related Products');
   });
 
-  // test('it should render multiple list elements', () => {
-  //   screen.findAllByRole('');
-  // });
+  test('it should render multiple list elements', async () => {
+    const items = await screen.findAllByRole('li');
+
+    expect(items.length).toBeGreaterThan(0);
+  });
 });
