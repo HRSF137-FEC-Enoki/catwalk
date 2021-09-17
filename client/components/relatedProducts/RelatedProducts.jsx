ea2411ccb98daa484e0efbc1cae0420ffa303221
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-17 11:12:19 -0700

    Remove duplicate html

--- client/components/relatedProducts/RelatedProducts.jsx
+++ client/components/relatedProducts/RelatedProducts.jsx
@@ -57,7 +57,6 @@ const RelatedProducts = ({ currentProduct, rating }) => {
 
   return (
     <div className="related-products">
-    <div className="related-products">
       <h3>Related Products</h3>
       <div className="related-products__row">
         {isLoading ? <div>Loadiing Related Products!</div>
