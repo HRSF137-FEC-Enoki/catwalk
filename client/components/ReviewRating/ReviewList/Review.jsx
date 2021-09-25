1b961f5690a37f29d23eda2c356fd0f24dfaec4b
runhui2010 <fredhuang128@yahoo.com>
2021-09-25 11:21:59 -0700

    remove moment

--- client/components/ReviewRating/ReviewList/Review.jsx
+++ client/components/ReviewRating/ReviewList/Review.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Moment from 'react-moment';
 import { AiOutlineCheck } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 
@@ -9,34 +8,44 @@ import Helpfulness from './Helpfulness';
 import StarRating from '../../StarRating';
 
 const STAR_SIZE = 16;
-const Review = ({ review, fetchReviews, fetchMeta }) => (
-  <div className="review">
-    <div className="rating_reviewer_date">
-      <StarRating size={STAR_SIZE} rating={review.rating} />
-      <div>
-        {review.reviewer_name}
-        <span>,&nbsp;</span>
-        <Moment format="MMMM Do, YYYY">{review.date}</Moment>
-      </div>
-    </div>
 
-    <div className="summary" data-testid="summary">{review.summary}</div>
-    <ReviewBody review={review} />
-    {review.recommend && (
-      <div className="recommend">
-        <AiOutlineCheck />
-        <span data-testid="recommend">I recommend this product</span>
+const Review = ({ review, fetchReviews, fetchMeta }) => {
+  const date = new Date(review.date);
+  const options = { month: 'long', year: 'numeric', day: 'numeric' };
+
+  return (
+    <div className="review">
+      <div className="rating_reviewer_date">
+        <StarRating size={STAR_SIZE} rating={review.rating} />
+        <div>
+          {review.reviewer_name}
+          <span>,&nbsp;</span>
+          <span>
+
+            {date.toLocaleString('en-US', options)}
+          </span>
+          {/* <Moment format="MMMM Do, YYYY">{review.date}</Moment> */}
+        </div>
       </div>
-    )}
-    {review.response && (
-      <div className="review_response">
-        <p>Response:</p>
-        <p data-testid="review_response">{review.response}</p>
-      </div>
-    )}
-    <Helpfulness review={review} fetchReviews={fetchReviews} fetchMeta={fetchMeta} />
-  </div>
-);
+
+      <div className="summary" data-testid="summary">{review.summary}</div>
+      <ReviewBody review={review} />
+      {review.recommend && (
+        <div className="recommend">
+          <AiOutlineCheck />
+          <span data-testid="recommend">I recommend this product</span>
+        </div>
+      )}
+      {review.response && (
+        <div className="review_response">
+          <p>Response:</p>
+          <p data-testid="review_response">{review.response}</p>
+        </div>
+      )}
+      <Helpfulness review={review} fetchReviews={fetchReviews} fetchMeta={fetchMeta} />
+    </div>
+  );
+};
 Review.propTypes = PropTypes.shape({
   review_name: PropTypes.string,
   date: PropTypes.string,
