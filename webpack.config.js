ef6a1830893d5c7d9065abfb9660166422c605a5
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-09-25 06:54:28 -0700

    seo 100 and code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'production',
   entry: './client/index.jsx',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'public'),
+    chunkFilename: '[id][chunkhash].js',
   },
-  plugins: [new CompressionPlugin({
-    test: /\.js(\?.*)?$/i,
-  })],
+  plugins: [
+    new CompressionPlugin({
+      test: /\.(js(\?.*)?|scss|html|svg)$/i,
+    }),
+    new WebpackBundleAnalyzer(),
+  ],
   module: {
     rules: [
       {
@@ -26,4 +31,15 @@ module.exports = {
     ],
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
